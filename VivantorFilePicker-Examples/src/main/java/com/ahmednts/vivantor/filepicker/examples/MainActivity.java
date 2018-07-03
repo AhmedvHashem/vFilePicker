380a8e65148a2302e5de3d68a2648bc9f7e0c676
Ahmed Hashem <ahmedhashemnts@gmail.com>
2018-07-04 00:29:23 +0200

    updating support lib

--- VivantorFilePicker-Examples/src/main/java/com/ahmednts/vivantor/filepicker/examples/MainActivity.java
+++ VivantorFilePicker-Examples/src/main/java/com/ahmednts/vivantor/filepicker/examples/MainActivity.java
@@ -1,81 +1,82 @@
 package com.ahmednts.vivantor.filepicker.examples;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-
 import com.ahmednts.vivantor.filepicker.VFileInfo;
 import com.ahmednts.vivantor.filepicker.VFilePicker;
 
-public class MainActivity extends AppCompatActivity
-{
-	private static final String TAG = MainActivity.class.getSimpleName();
+public class MainActivity
+    extends AppCompatActivity {
+  private static final String TAG = MainActivity.class.getSimpleName();
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-		PickupMediaFile();
-	}
+    PickupMediaFile();
+  }
 
-	void PickupMediaFile()
-	{
-		final CharSequence[] items = {"Gallery", "Camera", "Cancel"};
-		AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-		builder.setTitle("Select...");
-		builder.setItems(items, new DialogInterface.OnClickListener()
-		{
-			@Override
-			public void onClick(DialogInterface dialog, int item)
-			{
-				if (items[item].equals("Gallery"))
-				{
-					VFilePicker.getInstance().pick(VFilePicker.IMAGE).from(VFilePicker.GALLERY).saveTo("vFilePicker").show(MainActivity.this);
-				}
-				else if (items[item].equals("Camera"))
-				{
-					VFilePicker.getInstance().pick(VFilePicker.IMAGE).from(VFilePicker.CAMERA).saveTo("vFilePicker").show(MainActivity.this);
-				}
-				else if (items[item].equals("Cancel"))
-				{
-					dialog.dismiss();
-				}
-			}
-		});
-		builder.show();
-	}
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    VFilePicker.getInstance().destroyInstance();
+  }
 
-	@Override
-	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-	{
-		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+  void PickupMediaFile() {
+    final CharSequence[] items = { "Gallery", "Camera", "Cancel" };
+    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+    builder.setTitle("Select...");
+    builder.setItems(items, new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int item) {
+        if (items[item].equals("Gallery")) {
+          VFilePicker.getInstance()
+              .pick(VFilePicker.IMAGE)
+              .from(VFilePicker.GALLERY)
+              .saveTo("vFilePicker")
+              .show(MainActivity.this);
+        } else if (items[item].equals("Camera")) {
+          VFilePicker.getInstance()
+              .pick(VFilePicker.IMAGE)
+              .from(VFilePicker.CAMERA)
+              .saveTo("vFilePicker")
+              .show(MainActivity.this);
+        } else if (items[item].equals("Cancel")) {
+          dialog.dismiss();
+        }
+      }
+    });
+    builder.show();
+  }
 
-		VFilePicker.getInstance().onRequestPermissions(this, requestCode, permissions, grantResults);
-	}
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+      @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data)
-	{
-		super.onActivityResult(requestCode, resultCode, data);
+    VFilePicker.getInstance().onRequestPermissions(this, requestCode, permissions, grantResults);
+  }
 
-		VFilePicker.getInstance().onActivityResult(this, requestCode, resultCode, data);
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
 
-		VFileInfo fileInfo = VFilePicker.getInstance().getFileInfo();
+    VFilePicker.getInstance().onActivityResult(this, requestCode, resultCode, data);
 
-		if (fileInfo != null)
-		{
-			Log.d(TAG, "FileSize=" + fileInfo.getFileSize() + " Byte");
+    VFileInfo fileInfo = VFilePicker.getInstance().getFileInfo();
 
-			Log.d(TAG, "FileType=" + fileInfo.getFileType());
+    if (fileInfo != null) {
+      Log.d(TAG, "FileSize=" + fileInfo.getFileSize() + " Byte");
 
-			Log.d(TAG, "FileDuration=" + fileInfo.getFileDuration() + " sec");
-		}
-	}
+      Log.d(TAG, "FileType=" + fileInfo.getFileType());
+
+      Log.d(TAG, "FileDuration=" + fileInfo.getFileDuration() + " sec");
+    }
+  }
 }
--- VivantorFilePicker-Library/src/main/java/com/ahmednts/vivantor/filepicker/VFilePicker.java
+++ VivantorFilePicker-Library/src/main/java/com/ahmednts/vivantor/filepicker/VFilePicker.java
@@ -9,12 +9,13 @@ import android.os.Build;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-
+import android.support.v4.content.FileProvider;
 import java.io.File;
 
 /**
  * Created by AhmedNTS on 2015-09-07.
  */
+
 /**
  * Make sure to edit your activity`s "configChanges" for rotation changes
  * by adding this to your activity tag in your app Manifest.xml
@@ -37,305 +38,277 @@ file_paths.xml
     <external-path name="external_files" path="." />
 </paths>
  */
-public class VFilePicker
-{
-	public static final int IMAGE = 1000;
-	public static final int AUDIO = 2000;
-	public static final int VIDEO = 3000;
-	public static final int ANY = 4000;
+public class VFilePicker {
+  public static final int IMAGE = 1000;
+  public static final int AUDIO = 2000;
+  public static final int VIDEO = 3000;
+  public static final int ANY = 4000;
 
-	public static final int GALLERY = 10000;
-	public static final int CAMERA = 20000;
-	public static final int FILE_MANAGER = 30000;
+  public static final int GALLERY = 10000;
+  public static final int CAMERA = 20000;
+  public static final int FILE_MANAGER = 30000;
 
-	private final static int ANY_FILEMANAGER = 50;
-	private final static int IMAGE_FILEMANAGER = 51;
-	private final static int VIDEO_FILEMANAGER = 52;
-	private final static int AUDIO_FILEMANAGER = 53;
+  private final static int ANY_FILEMANAGER = 50;
+  private final static int IMAGE_FILEMANAGER = 51;
+  private final static int VIDEO_FILEMANAGER = 52;
+  private final static int AUDIO_FILEMANAGER = 53;
 
-	private final static int IMAGE_CAMERA = 11;
-	private final static int VIDEO_CAMERA = 12;
-	private final static int IMAGE_CAMERA_EXTERNAL = 20;
-	private final static int VIDEO_CAMERA_EXTERNAL = 21;
+  private final static int IMAGE_CAMERA = 11;
+  private final static int VIDEO_CAMERA = 12;
+  private final static int IMAGE_CAMERA_EXTERNAL = 20;
+  private final static int VIDEO_CAMERA_EXTERNAL = 21;
 
-	private final static int ANY_GALLERY = 30;
-	private final static int IMAGE_GALLERY = 31;
-	private final static int VIDEO_GALLERY = 32;
+  private final static int ANY_GALLERY = 30;
+  private final static int IMAGE_GALLERY = 31;
+  private final static int VIDEO_GALLERY = 32;
 
-	private int pickerType;
-	private int fromType;
+  private int pickerType;
+  private int fromType;
 
-	private String cameraDirectoryName;               // for camera with file only
-	private String filePath;
+  private String cameraDirectoryName;               // for camera with file only
+  private String filePath;
 
-	private static volatile VFilePicker instance;
+  private static volatile VFilePicker instance;
 
-	public synchronized static VFilePicker getInstance()
-	{
-		if (instance == null)
-			instance = new VFilePicker();
+  public synchronized static VFilePicker getInstance() {
+    if (instance == null) instance = new VFilePicker();
 
-		return instance;
-	}
+    return instance;
+  }
 
-	private VFilePicker()
-	{
-	}
+  public void destroyInstance() {
+    instance = null;
+  }
 
-	//till i create a builder
-	public VFilePicker pick(int pickerType)
-	{
-		this.pickerType = pickerType;
+  private VFilePicker() {
+  }
 
-		return instance;
-	}
+  //till i create a builder
+  public VFilePicker pick(int pickerType) {
+    this.pickerType = pickerType;
 
-	public VFilePicker from(int fromType)
-	{
-		this.fromType = fromType;
+    return instance;
+  }
 
-		return instance;
-	}
+  public VFilePicker from(int fromType) {
+    this.fromType = fromType;
 
-	public VFilePicker saveTo(String cameraDirectoryName)
-	{
-		this.cameraDirectoryName = cameraDirectoryName;
+    return instance;
+  }
 
-		return instance;
-	}
+  public VFilePicker saveTo(String cameraDirectoryName) {
+    this.cameraDirectoryName = cameraDirectoryName;
 
-	public void show(Activity context)
-	{
-		if (fromType == GALLERY)
-			FromGallery(context);
-		else if (fromType == CAMERA)
-		{
-			if (cameraDirectoryName == null || cameraDirectoryName.isEmpty())
-				FromCamera(context);
-			else
-				FromCameraWithFile(context);
-		}
-		else
-			FromFileManager(context);
-	}
+    return instance;
+  }
 
-	public VFileInfo getFileInfo()
-	{
-		if (filePath == null || filePath.isEmpty()) return null;
+  public void show(Activity context) {
+    if (fromType == GALLERY) {
+      FromGallery(context);
+    } else if (fromType == CAMERA) {
+      if (cameraDirectoryName == null || cameraDirectoryName.isEmpty()) {
+        FromCamera(context);
+      } else {
+        FromCameraWithFile(context);
+      }
+    } else {
+      FromFileManager(context);
+    }
+  }
 
-		return new VFileInfo(filePath);
-	}
+  public VFileInfo getFileInfo() {
+    if (filePath == null || filePath.isEmpty()) return null;
 
-	private void FromGallery(Activity context)
-	{
-		if (pickerType == AUDIO)
-			return;
+    return new VFileInfo(filePath);
+  }
 
-		String setType = "*/*";
-		int requestCode = ANY_GALLERY;
+  private void FromGallery(Activity context) {
+    if (pickerType == AUDIO) return;
 
-		if (pickerType == IMAGE)
-		{
-			setType = "image/*";
-			requestCode = VIDEO_GALLERY;
-		}
-		else if (pickerType == VIDEO)
-		{
-			setType = "video/*";
-			requestCode = IMAGE_GALLERY;
-		}
+    String setType = "*/*";
+    int requestCode = ANY_GALLERY;
 
-		if (RequestPermissions(context, requestCode))
-		{
-			Intent intent = new Intent(Intent.ACTION_PICK);
-			intent.setType(setType);
-			intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-			context.startActivityForResult(intent, requestCode);
-		}
-	}
+    if (pickerType == IMAGE) {
+      setType = "image/*";
+      requestCode = VIDEO_GALLERY;
+    } else if (pickerType == VIDEO) {
+      setType = "video/*";
+      requestCode = IMAGE_GALLERY;
+    }
 
-	private void FromCamera(Activity context)
-	{
-		if (pickerType == AUDIO || pickerType == ANY) return;
+    if (RequestPermissions(context, requestCode)) {
+      Intent intent = new Intent(Intent.ACTION_PICK);
+      intent.setType(setType);
+      intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+      context.startActivityForResult(intent, requestCode);
+    }
+  }
 
-		String action = MediaStore.ACTION_IMAGE_CAPTURE;
-		int requestCode = IMAGE_CAMERA;
+  private void FromCamera(Activity context) {
+    if (pickerType == AUDIO || pickerType == ANY) return;
 
-		if (pickerType == IMAGE)
-		{
-			action = MediaStore.ACTION_IMAGE_CAPTURE;
-			requestCode = IMAGE_CAMERA;
-		}
-		else if (pickerType == VIDEO)
-		{
-			action = MediaStore.ACTION_VIDEO_CAPTURE;
-			requestCode = VIDEO_CAMERA;
-		}
+    String action = MediaStore.ACTION_IMAGE_CAPTURE;
+    int requestCode = IMAGE_CAMERA;
 
-		if (RequestPermissions(context, requestCode))
-		{
-			Intent intent = new Intent(action);
-			context.startActivityForResult(intent, requestCode);
-		}
-	}
+    if (pickerType == IMAGE) {
+      action = MediaStore.ACTION_IMAGE_CAPTURE;
+      requestCode = IMAGE_CAMERA;
+    } else if (pickerType == VIDEO) {
+      action = MediaStore.ACTION_VIDEO_CAPTURE;
+      requestCode = VIDEO_CAMERA;
+    }
 
-	private void FromCameraWithFile(Activity context)
-	{
-		if (pickerType == AUDIO || pickerType == ANY) return;
-		if (cameraDirectoryName == null || cameraDirectoryName.isEmpty()) return;
+    if (RequestPermissions(context, requestCode)) {
+      Intent intent = new Intent(action);
+      context.startActivityForResult(intent, requestCode);
+    }
+  }
 
-		String action = MediaStore.ACTION_IMAGE_CAPTURE;
-		int requestCode = IMAGE_CAMERA_EXTERNAL;
+  private void FromCameraWithFile(Activity context) {
+    if (pickerType == AUDIO || pickerType == ANY) return;
+    if (cameraDirectoryName == null || cameraDirectoryName.isEmpty()) return;
 
-		if (pickerType == IMAGE)
-		{
-			action = MediaStore.ACTION_IMAGE_CAPTURE;
-			requestCode = IMAGE_CAMERA_EXTERNAL;
-		}
-		else if (pickerType == VIDEO)
-		{
-			action = MediaStore.ACTION_VIDEO_CAPTURE;
-			requestCode = VIDEO_CAMERA_EXTERNAL;
-		}
+    String action = MediaStore.ACTION_IMAGE_CAPTURE;
+    int requestCode = IMAGE_CAMERA_EXTERNAL;
 
-		if (RequestPermissions(context, requestCode))
-		{
-			filePath = Utils.GenerateFilePath(cameraDirectoryName, pickerType == IMAGE ? 1 : 3);
+    if (pickerType == IMAGE) {
+      action = MediaStore.ACTION_IMAGE_CAPTURE;
+      requestCode = IMAGE_CAMERA_EXTERNAL;
+    } else if (pickerType == VIDEO) {
+      action = MediaStore.ACTION_VIDEO_CAPTURE;
+      requestCode = VIDEO_CAMERA_EXTERNAL;
+    }
 
-			if (filePath == null) return;
+    if (RequestPermissions(context, requestCode)) {
+      filePath = Utils.GenerateFilePath(cameraDirectoryName, pickerType == IMAGE ? 1 : 3);
 
-			//Intent intent = new Intent(action);
-			//intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(filePath)));
-			//context.startActivityForResult(intent, requestCode);
-			Intent intent = new Intent(action);
-			Uri photoURI;
-			if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP)
-				photoURI = FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".fileprovider", new File(filePath));
-			else
-				photoURI = Uri.fromFile(new File(filePath));
+      if (filePath == null) return;
 
-			intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, photoURI);
-			intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-			if (intent.resolveActivity(context.getPackageManager()) != null)
-				context.startActivityForResult(intent, requestCode);
-			
-		}
-	}
+      //Intent intent = new Intent(action);
+      //intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(filePath)));
+      //context.startActivityForResult(intent, requestCode);
+      Intent intent = new Intent(action);
+      Uri photoURI;
+      if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+        photoURI = FileProvider.getUriForFile(context,
+            context.getApplicationContext().getPackageName() + ".fileprovider", new File(filePath));
+      } else {
+        photoURI = Uri.fromFile(new File(filePath));
+      }
 
-	private void FromFileManager(Activity context)
-	{
-		String setType = "*/*";
-		int requestCode = ANY_FILEMANAGER;
+      intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, photoURI);
+      intent.addFlags(
+          Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+      if (intent.resolveActivity(context.getPackageManager()) != null) {
+        context.startActivityForResult(intent, requestCode);
+      }
+    }
+  }
 
-		if (pickerType == ANY)
-		{
-			if (Build.VERSION.SDK_INT >= 19)
-				setType = "*/*";
-			else
-				setType = "file/*";
+  private void FromFileManager(Activity context) {
+    String setType = "*/*";
+    int requestCode = ANY_FILEMANAGER;
 
-			requestCode = ANY_FILEMANAGER;
-		}
-		else if (pickerType == IMAGE)
-		{
-			setType = "image/*";
-			requestCode = IMAGE_FILEMANAGER;
-		}
-		else if (pickerType == VIDEO)
-		{
-			setType = "video/*";
-			requestCode = VIDEO_FILEMANAGER;
-		}
-		else if (pickerType == AUDIO)
-		{
-			setType = "audio/*";
-			requestCode = AUDIO_FILEMANAGER;
-		}
+    if (pickerType == ANY) {
+      if (Build.VERSION.SDK_INT >= 19) {
+        setType = "*/*";
+      } else {
+        setType = "file/*";
+      }
 
-		if (RequestPermissions(context, requestCode))
-		{
-			Intent intent = new Intent();
-			if (Build.VERSION.SDK_INT >= 19)
-			{
-				// For Android KitKat, we use a different intent to ensure
-				// we can get the file path from the returned intent URI
-				intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
-				intent.addCategory(Intent.CATEGORY_OPENABLE);
-				intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-			}
-			else
-			{
-				intent.setAction(Intent.ACTION_GET_CONTENT);
-			}
+      requestCode = ANY_FILEMANAGER;
+    } else if (pickerType == IMAGE) {
+      setType = "image/*";
+      requestCode = IMAGE_FILEMANAGER;
+    } else if (pickerType == VIDEO) {
+      setType = "video/*";
+      requestCode = VIDEO_FILEMANAGER;
+    } else if (pickerType == AUDIO) {
+      setType = "audio/*";
+      requestCode = AUDIO_FILEMANAGER;
+    }
 
-			intent.setType(setType);
-			context.startActivityForResult(intent, requestCode);
-		}
-	}
+    if (RequestPermissions(context, requestCode)) {
+      Intent intent = new Intent();
+      if (Build.VERSION.SDK_INT >= 19) {
+        // For Android KitKat, we use a different intent to ensure
+        // we can get the file path from the returned intent URI
+        intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+      } else {
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+      }
 
-	private boolean RequestPermissions(Activity context, int requestCode)
-	{
-		boolean permissionGranted = true;
+      intent.setType(setType);
+      context.startActivityForResult(intent, requestCode);
+    }
+  }
 
-		String[] cameraPermissions = new String[]
-				{
-						Manifest.permission.CAMERA,
-						Manifest.permission.WRITE_EXTERNAL_STORAGE
-				};
+  private boolean RequestPermissions(Activity context, int requestCode) {
+    boolean permissionGranted = true;
 
-		String[] galleryPermissions = new String[]
-				{
-						Manifest.permission.WRITE_EXTERNAL_STORAGE
-				};
+    String[] cameraPermissions = new String[] {
+        Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-		{
-			if (requestCode == IMAGE_CAMERA || requestCode == VIDEO_CAMERA)
-				permissionGranted = ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
-						&& ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-			else
-				permissionGranted = ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    String[] galleryPermissions = new String[] {
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
-			if (!permissionGranted)
-			{
-				if (requestCode == IMAGE_CAMERA || requestCode == IMAGE_CAMERA_EXTERNAL || requestCode == VIDEO_CAMERA || requestCode == VIDEO_CAMERA_EXTERNAL)
-					ActivityCompat.requestPermissions(context, cameraPermissions, requestCode);
-				else
-					ActivityCompat.requestPermissions(context, galleryPermissions, requestCode);
-			}
-		}
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      if (requestCode == IMAGE_CAMERA || requestCode == VIDEO_CAMERA) {
+        permissionGranted = ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+            == PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(context,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+      } else {
+        permissionGranted =
+            ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+      }
 
-		return permissionGranted;
-	}
+      if (!permissionGranted) {
+        if (requestCode == IMAGE_CAMERA
+            || requestCode == IMAGE_CAMERA_EXTERNAL
+            || requestCode == VIDEO_CAMERA
+            || requestCode == VIDEO_CAMERA_EXTERNAL) {
+          ActivityCompat.requestPermissions(context, cameraPermissions, requestCode);
+        } else {
+          ActivityCompat.requestPermissions(context, galleryPermissions, requestCode);
+        }
+      }
+    }
 
-	public void onRequestPermissions(Activity context, int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-	{
-		if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-		{
-			if (requestCode == IMAGE_GALLERY || requestCode == VIDEO_GALLERY || requestCode == ANY_GALLERY)
-				FromGallery(context);
-			else if (requestCode == IMAGE_CAMERA || requestCode == VIDEO_CAMERA)
-				FromCamera(context);
-			else if (requestCode == IMAGE_CAMERA_EXTERNAL || requestCode == VIDEO_CAMERA_EXTERNAL)
-				FromCameraWithFile(context);
-			else if (requestCode >= ANY_FILEMANAGER && requestCode <= AUDIO_FILEMANAGER)
-				FromFileManager(context);
-		}
-	}
+    return permissionGranted;
+  }
 
-	public void onActivityResult(Activity context, int requestCode, int resultCode, Intent data)
-	{
-		if (resultCode == Activity.RESULT_OK)
-		{
-			if (requestCode != IMAGE_CAMERA_EXTERNAL && requestCode != VIDEO_CAMERA_EXTERNAL)
-			{
-				if (data.getData() == null)
-					return;
+  public void onRequestPermissions(Activity context, int requestCode, @NonNull String[] permissions,
+      @NonNull int[] grantResults) {
+    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+      if (requestCode == IMAGE_GALLERY
+          || requestCode == VIDEO_GALLERY
+          || requestCode == ANY_GALLERY) {
+        FromGallery(context);
+      } else if (requestCode == IMAGE_CAMERA || requestCode == VIDEO_CAMERA) {
+        FromCamera(context);
+      } else if (requestCode == IMAGE_CAMERA_EXTERNAL || requestCode == VIDEO_CAMERA_EXTERNAL) {
+        FromCameraWithFile(context);
+      } else if (requestCode >= ANY_FILEMANAGER && requestCode <= AUDIO_FILEMANAGER) {
+        FromFileManager(context);
+      }
+    }
+  }
 
-				filePath = Utils.getFilePathFromURI(context, data.getData());
-			}
-		}
-	}
+  public void onActivityResult(Activity context, int requestCode, int resultCode, Intent data) {
+    if (resultCode == Activity.RESULT_OK) {
+      if (requestCode != IMAGE_CAMERA_EXTERNAL && requestCode != VIDEO_CAMERA_EXTERNAL) {
+        if (data.getData() == null) return;
+
+        filePath = Utils.getFilePathFromURI(context, data.getData());
+      }
+    }
+  }
 }
 
 
